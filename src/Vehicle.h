6294336d1728979519eab44937a37fc075ab27d5
mvirgo <mvirgs44@gmail.com>
2021-04-28 18:21:48 -0500

    feat: add vehicle states for determining destination to set

--- src/Vehicle.h
+++ src/Vehicle.h
@@ -8,18 +8,27 @@
 
 class Passenger;
 
+enum VehicleState {
+    no_passenger_queued,
+    passenger_queued,
+    driving_passenger,
+};
+
 class Vehicle: public MapObject {
   public:
     // constructor / destructor
 
     // getters / setters
     int Shape() { return shape_; }
+    int State() { return state_; }
+    void SetState(VehicleState state) { state_ = state; }
     std::vector<RouteModel::Node> Path() { return path_; }
     void SetPath(std::vector<RouteModel::Node> path) { path_ = path; }
 
   private:
     std::shared_ptr<Passenger> passenger_;
     int shape_ = DrawMarker::square;
+    int state_ = VehicleState::no_passenger_queued;
     std::vector<RouteModel::Node> path_;
     // TODO: Functions for sim/driving and handling passenger requests
 };
--- src/VehicleManager.cpp
+++ src/VehicleManager.cpp
@@ -11,16 +11,20 @@ void VehicleManager::GenerateNew() {
     // TODO: Add appropriate handling of Vehicle to avoid memory leaks if made a pointer
     // Get random start position
     auto start = model_->GetRandomMapPosition();
-    // Find the nearest road node to start position
-    auto nearest = model_->FindClosestNode(start[0], start[1]);
-    // Set road position and id of vehicle
+    // Set a random destination until they have a passenger to go pick up
+    auto destination = model_->GetRandomMapPosition();
+    // Find the nearest road node to start and destination positions
+    auto nearest_start = model_->FindClosestNode(start[0], start[1]);
+    auto nearest_dest = model_->FindClosestNode(destination[0], destination[1]);
+    // Set road position, destination and id of vehicle
     Vehicle vehicle = Vehicle(); // TODO: May need change for memory changes later
-    vehicle.SetPosition(nearest.x, nearest.y);
+    vehicle.SetPosition(nearest_start.x, nearest_start.y);
+    vehicle.SetDestination(nearest_dest.x, nearest_dest.y);
     vehicle.SetId(idCnt_++);
     vehicles_.emplace_back(vehicle);
     // Output id and location of vehicle looking to give rides
     std::lock_guard<std::mutex> lck(mtx_);
-    std::cout << "Vehicle ID#" << idCnt_ - 1 << " now driving from: " << nearest.y << ", " << nearest.x << "." << std::endl;
+    std::cout << "Vehicle ID#" << idCnt_ - 1 << " now driving from: " << nearest_start.y << ", " << nearest_start.x << "." << std::endl;
 }
 
 void VehicleManager::Simulate() {
